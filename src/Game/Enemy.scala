8a08ca24ccf3f594f1d3dba3e5670cb6c6c7aaa6
Markus Regardh <markus.regardh@aalto.fi>
2020-03-06 11:55:22 +0200

    Perusluokat tehty, vielä muutama funktio puuttuu

--- src/Game/Enemy.scala
+++ src/Game/Enemy.scala
@@ -1,5 +1,66 @@
 package Game
+import o1._
+import TDGUI.TD._
 
-class Enemy {
+class Enemy(initialHP: Int, start: GridPos, speed: Int) {
   
+var hp = initialHP
+
+var v = speed
+
+var tickC = 0
+
+var gridLocation = start
+
+var x = toPixelPos(start).x
+
+var y = toPixelPos(start).y
+
+var pLocation = toPixelPos(start)
+
+
+
+
+var dir = South
+  
+def dead = (hp<=0) 
+
+def move(d: CompassDir) = {
+  if (d == South) {
+    y += speed
+  } else if (d == North) {
+    y -= speed
+  } else if (d == East) {
+    x += speed
+  } else if (d == West) {
+    x -= speed
+  }
+  
+  pLocation = new Pos(x,y)
+}
+
+
+def dealDmg(map: Map) = {
+  if (gridLocation == map.goal) {
+    this.hp=0
+    1
+} else {
+  0
+}
+}
+
+def moveForward(map: Map) = {
+  while (!map.path.contains(gridLocation.neighbor(dir))){
+    dir = dir.clockwise
+    if (!map.path.contains(gridLocation.neighbor(dir))){
+      dir = dir.counterClockwise.counterClockwise
+    }
+  }
+  tickC += 1
+  move(dir)
+  if (tickC%20 == 0) {
+    gridLocation = gridLocation.neighbor(dir)
+    tickC = 0
+  }
+}
 }
--- src/Game/Tower.scala
+++ src/Game/Tower.scala
@@ -1,10 +1,80 @@
 package Game
 import o1._
+import scala.collection.mutable.Buffer
 
-abstract class Tower(Color: Color, initialDmg: Int, name: String, range: Int) {
+abstract class Tower(loc: GridPos) {
+  
+  val color: Color
+  var dmg: Int
+  val name: String
+  var range: Int
+  var fireRate: Double
+  var target: Option[Enemy]
+  var tickCounter: Int
+  val location: GridPos
   
 def upgrade: Unit
 
-def shoot(enemy: Enemy): Unit
+def shoot: Unit
+
+def chooseTarget(enemies: Buffer[Enemy]): Unit
+
+}
+
+class Sniper(loc: GridPos) extends Tower(loc){
+ 
+var dmg = 50
+val color= Red
+val name = "Sniper"
+var range = 100000
+var fireRate = 30
+var target: Option[Enemy] = None
+var tickCounter= 0
+val location = loc
+
+def upgrade = {
+  dmg = dmg * 2
+}
+
+def chooseTarget(enemies: Buffer[Enemy]) = {
+   target = enemies.filter(range >= _.gridLocation.distance(this.location)).headOption
+  }
+
+def shoot= {
+  if (tickCounter%fireRate == 0) {
+    target match {
+      case Some(enemy) => enemy.hp = enemy.hp - dmg
+      case None => 
+    }
+    tickCounter = 1
+  }
+}
+}
+
+class Classic(loc: GridPos) extends Tower(loc){
+  var dmg = 15
+  val color = Blue
+  val name = "Classic"
+  var range = 10
+  var fireRate = 10
+  var target: Option[Enemy] = None
+  var tickCounter= 0
+  val location = loc
   
+  def chooseTarget(enemies: Buffer[Enemy]) = {
+   target = enemies.filter(range >= _.gridLocation.distance(this.location)).headOption
+  }
+  
+  def upgrade = {
+    fireRate = fireRate*2
+  }
+  def shoot= {
+  if (tickCounter%fireRate == 0) {
+    target match {
+      case Some(enemy) => enemy.hp = enemy.hp - dmg
+      case None => 
+    }
+    tickCounter = 1
+  }
+}
 }
